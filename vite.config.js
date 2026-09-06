37c4475fcea107c3b6f712defe3c2d920d31b97d
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-06 15:08:48 -0300

    Enable integrated signal feedback engine

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ import targetDuziaPatch from './vite-target-duzia-patch.js'
 import focusRepetitionPatch from './vite-focus-repetition.js'
 import terminalFilterPatch from './vite-terminal-filter.js'
 import last3ClickFilterPatch from './vite-last3-click-filter.js'
+import signalFeedbackPatch from './vite-signal-feedback.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), signalFeedbackPatch(), react()],
 })
