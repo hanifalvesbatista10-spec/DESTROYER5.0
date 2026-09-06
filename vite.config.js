0a0b986797d92f33e6ebc165fdfc89addcf499f6
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-06 14:20:57 -0300

    feat: require dozen confirmation in 5 and 10 result windows

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import destroyerPatch from './vite-destroyer-patch.js'
 import radarCleanup from './vite-radar-cleanup.js'
+import targetDuziaPatch from './vite-target-duzia-patch.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), react()],
 })
