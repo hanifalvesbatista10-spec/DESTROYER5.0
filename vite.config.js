c6085e4d03de0062bfcd4c07d845dbcf108254be
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-06 13:53:50 -0300

    fix: enable cleaned house repetition radar

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import destroyerPatch from './vite-destroyer-patch.js'
+import radarCleanup from './vite-radar-cleanup.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), react()],
 })
