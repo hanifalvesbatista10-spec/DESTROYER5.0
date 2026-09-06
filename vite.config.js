15308be27a7f79440b10223ff90ba4af73632453
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-06 14:43:56 -0300

    enable clickable last-3 filter patch

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import radarCleanup from './vite-radar-cleanup.js'
 import targetDuziaPatch from './vite-target-duzia-patch.js'
 import focusRepetitionPatch from './vite-focus-repetition.js'
 import terminalFilterPatch from './vite-terminal-filter.js'
+import last3ClickFilterPatch from './vite-last3-click-filter.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), react()],
 })
