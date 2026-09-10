88197735458c5372c4a95107f6231ad10cc05930
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-09 21:02:12 -0300

    Enable terminal strategy backtest patch

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,9 @@ import directedAnalysisPatch from './vite-directed-analysis.js'
 import directedWheelRefinePatch from './vite-directed-wheel-refine.js'
 import directedAnalysisHistoryLockPatch from './vite-directed-analysis-history-lock.js'
 import terminalSideAlertPatch from './vite-terminal-side-alert.js'
+import terminalStrategyBacktestPatch from './vite-terminal-strategy-backtest.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), react()],
 })
