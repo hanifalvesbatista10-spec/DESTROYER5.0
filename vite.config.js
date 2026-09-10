044cccff145a908bd7d19cf553a2b076a5caa439
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-09 21:39:47 -0300

    Enable terminal visual quality patch

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,9 @@ import terminalSideAlertPatch from './vite-terminal-side-alert.js'
 import terminalStrategyBacktestPatch from './vite-terminal-strategy-backtest.js'
 import terminalNeighborWeightPatch from './vite-terminal-neighbor-weight.js'
 import backtestTogglePatch from './vite-backtest-toggle.js'
+import terminalVisualQualityPatch from './vite-terminal-visual-quality.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), terminalNeighborWeightPatch(), backtestTogglePatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), terminalNeighborWeightPatch(), backtestTogglePatch(), terminalVisualQualityPatch(), react()],
 })
