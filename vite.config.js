27e41d236c8646d8b87435e7a015da17b5c04753
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-11 11:21:03 -0300

    feat: enable streets analysis focus mode

--- vite.config.js
+++ vite.config.js
@@ -19,8 +19,9 @@ import terminalVisualQualityPatch from './vite-terminal-visual-quality.js'
 import terminalSpaceCleanupPatch from './vite-terminal-space-cleanup.js'
 import terminalActivationNowPatch from './vite-terminal-activation-now.js'
 import cdFocusModePatch from './vite-cd-focus-mode.js'
+import ruasFocusModePatch from './vite-ruas-focus-mode.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), terminalNeighborWeightPatch(), backtestTogglePatch(), terminalVisualQualityPatch(), terminalSpaceCleanupPatch(), terminalActivationNowPatch(), cdFocusModePatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), terminalNeighborWeightPatch(), backtestTogglePatch(), terminalVisualQualityPatch(), terminalSpaceCleanupPatch(), terminalActivationNowPatch(), cdFocusModePatch(), ruasFocusModePatch(), react()],
 })
