b587d70a62712e8f01a8154160c54ae9a623da92
hanifalvesbatista10-spec <hanif.alves.batista10@gmail.com>
2026-09-11 11:51:33 -0300

    Enable Foco Rua refinement

--- vite.config.js
+++ vite.config.js
@@ -20,8 +20,9 @@ import terminalSpaceCleanupPatch from './vite-terminal-space-cleanup.js'
 import terminalActivationNowPatch from './vite-terminal-activation-now.js'
 import cdFocusModePatch from './vite-cd-focus-mode.js'
 import ruasFocusModePatch from './vite-ruas-focus-mode.js'
+import ruaModeRefinePatch from './vite-rua-mode-refine.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), terminalNeighborWeightPatch(), backtestTogglePatch(), terminalVisualQualityPatch(), terminalSpaceCleanupPatch(), terminalActivationNowPatch(), cdFocusModePatch(), ruasFocusModePatch(), react()],
+  plugins: [destroyerPatch(), radarCleanup(), targetDuziaPatch(), focusRepetitionPatch(), terminalFilterPatch(), last3ClickFilterPatch(), hiddenCharacteristicsPatch(), signalFeedbackPatch(), directedAnalysisPatch(), directedWheelRefinePatch(), directedAnalysisHistoryLockPatch(), terminalSideAlertPatch(), terminalStrategyBacktestPatch(), terminalNeighborWeightPatch(), backtestTogglePatch(), terminalVisualQualityPatch(), terminalSpaceCleanupPatch(), terminalActivationNowPatch(), cdFocusModePatch(), ruasFocusModePatch(), ruaModeRefinePatch(), react()],
 })
